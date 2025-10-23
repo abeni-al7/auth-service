ecf0c86242966fffaa5da01c0ebf7bbb1d29ee8d
abeni-al7 <abenezeralebachew3@gmail.com>
2025-10-23 10:48:10 +0300

    [chore] Use the Singleton pattern to instantiate global app config

--- src/config/config.py
+++ src/config/config.py
@@ -1,5 +1,5 @@
 import os
-from functools import lru_cache
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -11,11 +11,11 @@ class Settings(BaseSettings):
     (e.g., .env.dev) determined by the `ENVIRONMENT` variable.
     """
     APP_NAME: str = "Auth service"
-    PORT: int
+    PORT: int = Field(ge=1, le=65535)
     DEBUG: bool = False
     DATABASE_URI: str
-    JWT_SECRET_KEY: str
-    JWT_EXPIRY_SECONDS: int
+    JWT_SECRET_KEY: str = Field(min_length=32)
+    JWT_EXPIRY_SECONDS: int = Field(gt=0)
 
     model_config = SettingsConfigDict(
         env_file=f".env.{os.getenv("ENVIRONMENT", "dev")}",
@@ -23,12 +23,14 @@ class Settings(BaseSettings):
     )
 
 
-settings = Settings()
+_settings: Settings | None = None
 
 
-@lru_cache
 def get_settings() -> Settings:
     """
-    Return a cached instance of the application settings.
-    """
-    return settings
+     Return a cached instance of the application settings.
+     """
+    global _settings
+    if _settings is None:
+        _settings = Settings()
+    return _settings
